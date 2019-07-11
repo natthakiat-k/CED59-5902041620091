5a38abed95b706215abfb16e8791869ad01aa41c
Natthakiat Khunkaew <s5902041620091@email.kmutnb.ac.th>
2019-07-11 14:39:10 +0700

    add files for assignment3

---
+++ assignment3/func.py
@@ -0,0 +1,5 @@
+def return_42():
+  return 42
+
+def my_function(x1, x2):
+  return x1 * x2
---
+++ assignment3/main.py
@@ -0,0 +1,4 @@
+from func import *
+
+print("Result of return_42() is ", return_42());
+print("Result of my_method(x1, x2) is ", my_method(5, 6));
