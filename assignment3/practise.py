7f7d677a7274b5bed9b3e16e9d36b835a5def76e
k.natthakiat <52273921+natthakiat-k@users.noreply.github.com>
2019-07-11 17:22:44 +0700

    Create practise.py

---
+++ assignment3/practise.py
@@ -0,0 +1,15 @@
+my_list = [97, 1, 2]
+print("===== LIST =====")
+print("LIST ---> ", my_list)
+print("my_list : ", sum(my_list))
+print()
+
+my_tuple = (9, 5 ,1 ,91, 92)
+print("===== TUPLE =====")
+print("my_tuple : ", my_tuple)
+print()
+
+my_set1 = {5, 77, 8, 0, 19, 12, 9, 61, 45}
+my_set2 = {9, 12, 77, 63, 74, 5, 12 ,3}
+print("===== SET =====")
+print("my_set : ", my_set1.intersection(my_set2))
