f11a720b9e91249b9c81fde103aa1064f5293a48
natthakiat-k <s5902041620091@email.kmutnb.ac.th>
2019-08-21 23:51:49 +0700

    Flask-WTF

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+from wtforms import StringField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'AOFMONKY'
+
+class WTFform(FlaskForm):
+    first_name = StringField("first_name", validators=[InputRequired()])
+    last_name = StringField("last_name", validators=[InputRequired()])
+    email = StringField("email", [InputRequired(""), Email("example@gmail.com")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, message="Password least 8 digits")])
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = WTFform()
+    return render_template('form.html', form=form)
 
-@app.route('/', methods=["POST"])
-def register():
-    return 'Done!'
+@app.route('/registor', methods=["post"])
+def registor():
+    form = WTFform()
+    if form.validate_on_submit():
+        return "Registor SUCCESS!"
+    return render_template('form.html', form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
