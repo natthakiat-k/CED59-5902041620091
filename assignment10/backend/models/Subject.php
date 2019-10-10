ab367e33be9ec49951f63e4bce485102888b8151
natthakiat-k <s5902041620091@email.kmutnb.ac.th>
2019-10-10 10:53:55 +0700

    Running migrate table

---
+++ assignment10/backend/models/Subject.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace backend\models;
+
+use yii\behaviors\TimestampBehavior;
+use yii\behaviors\BlameableBehavior;
+
+class Subject extends \common\models\Subject {
+
+    public function behaviors() {
+        return [
+            TimestampBehavior::class,
+            BlameableBehavior::class
+        ];
+    }
+
+    public function rules() {
+        return [
+            ['name', 'required', 'message' => 'please insert data'],
+            ['name', 'email', 'message' => 'please insert type example@email.com']
+        ];
+    }
+}
